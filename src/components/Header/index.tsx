4fd8180653bba270790ad1815062ce05fb42eb9c
Caio Vinicius <caiochagas77@hotmail.com>
2021-06-16 14:48:29 -0300

    first commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return(
+    <header className={styles.headerContainer}>
+
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="spacetraveling"/>
+        <img src="/images/spacetraveling.svg" alt="spacetraveling"/>
+        <span>.</span>
+
+      </div>
+
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
 
+// import Link from 'next/link'
+
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,9 +26,37 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+ export default function Home() {
+  return(
+    <main className={styles.container}>
+      <div className={styles.posts}>
+        <a>
+        <strong>Como utilizar Hooks</strong>
+        <p>Pensando em sincronização ao invés de ciclos de vida.</p>
+        <time>15 Mar 2021 <span> Danilo Vieira</span></time>
+
+        </a>
+
+        <a>
+        <strong>Como utilizar Hooks</strong>
+        <p>Pensando em sincronização ao invés de ciclos de vida.</p>
+        <time>15 Mar 2021 <span> Danilo Vieira</span></time>
+
+        </a>
+
+        <a>
+        <strong>Como utilizar Hooks</strong>
+        <p>Pensando em sincronização ao invés de ciclos de vida.</p>
+        <time>15 Mar 2021 <span> Danilo Vieira</span></time>
+
+        </a>
+
+      </div>
+
+      <p className={styles.loadMore}>Carregar mais posts</p>
+    </main>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
